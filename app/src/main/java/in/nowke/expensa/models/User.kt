b7395a8d2c600d01cb14bf823fbe47e5a78574b3
Navaneesh Kumar <navaneeshkumarb@gmail.com>
2017-07-25 23:46:21 +0530

    Refactor Gradle

--- app/src/main/java/in/nowke/expensa/models/User.kt
+++ app/src/main/java/in/nowke/expensa/models/User.kt
@@ -2,9 +2,6 @@ package `in`.nowke.expensa.models
 
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
-import android.arch.persistence.room.ColumnInfo
-import android.text.format.DateUtils
-import java.util.*
 
 /**
  * User model
